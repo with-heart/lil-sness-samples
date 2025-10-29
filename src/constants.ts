8e872c17a6dac92fd1ed516a5c7a2082f85a8fd9
with-heart <with.heart+git@pm.me>
2025-10-29 17:51:26 -0400

    fix broken _base url

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 /** The `_base` value for `strudel.json`. */
-export const BASE_URL = `https://raw.githubusercontent.com/with-heart/lil-sness-samples/main/samples/`
+export const BASE_URL = `https://raw.githubusercontent.com/with-heart/lil-sness-samples/main/`
 
 /** Prefix for all `strudel.json` keys. */
 export const KEY_PREFIX = 'snes'
